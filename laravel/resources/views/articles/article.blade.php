5223ba34a2f25205b8cbb692106f8e88746c19f3
misaki toyama <misakitoyama@misakinoMacBook-Pro.local>
2021-03-23 08:18:10 +0900

    log

--- laravel/resources/views/articles/article.blade.php
+++ laravel/resources/views/articles/article.blade.php
@@ -1,6 +1,6 @@
 @extends('layout.common')
  
-@section('title', 'インデックスページ')
+@section('title', 'log')
 <!-- @section('keywords', 'キーワード1,キーワード2,キーワード3')
 @section('description', 'インデックスページの説明文です') -->
 <!-- @section('pageCss') -->
