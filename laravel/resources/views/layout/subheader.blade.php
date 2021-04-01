9996f96c6694dd3aa1fb706fe2414ccea1e7943e
misaki toyama <misakitoyama@misakinoMacBook-Pro.local>
2021-04-01 15:46:36 +0900

    icon morebig

--- laravel/resources/views/layout/subheader.blade.php
+++ laravel/resources/views/layout/subheader.blade.php
@@ -6,16 +6,16 @@
     <div class="row text-center justify-content-center">
       <div class="col-12 col-sm-2 m-auto  pt-4 pt-sm-0">
         <a class="text-decoration-none text-reset" href="{{ action('ArticleController@index') }}">
-          <i class="fas fa-home fa-4x text-light"></i>
-          <h3><strong>HOME</strong></h3>
+          <i class="fas fa-home fa-3x text-light"></i>
+          <h6><strong>HOME</strong></h6>
         </a>
       </div>
 
       @foreach($category_lists as $category_list)
       <div class="col-12 col-sm-2 m-auto pt-4 pt-sm-0">
         <a class="text-decoration-none  text-reset" href="{{ url('blog/' . $category_list->slug ) }}">
-          <i class="{{  $icon[$loop->index] }} fa-4x text-light"></i>
-          <h3><strong>{{ $category_list->name }}</strong></h3>
+          <i class="{{  $icon[$loop->index] }} fa-3x text-light"></i>
+          <h6><strong>{{ $category_list->name }}</strong></h6>
         </a>
       </div>
       @endforeach
