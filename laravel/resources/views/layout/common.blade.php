516f9586730af55afa6a71d6d5d3ad3f7e183edb
misaki toyama <misakitoyama@misakinoMacBook-Pro.local>
2021-04-01 09:02:49 +0900

    レスポンシブメタタグ追加

--- laravel/resources/views/layout/common.blade.php
+++ laravel/resources/views/layout/common.blade.php
@@ -2,8 +2,9 @@
 <html lang="ja">
 <head>
 <meta charset="UTF-8">
-<title>@yield('title')</title>
 <meta name="csrf-token" content="{{ csrf_token() }}">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>@yield('title')</title>
 
 <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
 <link rel="stylesheet" href="{{ asset('/css/froala.css') }}">
